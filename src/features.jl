8a1110a59fd66df3171143c7c47d43814569736b
David Dohan <ddohan@cs.princeton.edu>
2016-04-13 23:44:44 -0700

    Basic features (first pass), training data utils

---
+++ src/features.jl
@@ -0,0 +1,99 @@
+## Feature extraction
+# All features calculated relative to current player to move
+
+function one_hot(counts::Array{Int, 2}; maxval=8)
+    out = BitArray(N, N, maxval)
+    for i in 1:(maxval-1)
+        out[:, :, i] = counts == i
+    end
+    out[:, :, maxval] = counts .>= maxval
+    out
+end
+
+#
+function stone_color(board::Board)
+    result = BitArray(N, N, 3)
+    cp = current_player(board)
+    result[:, :, 1] = board.board .== cp
+    result[:, :, 2] = board.board .== -cp
+    result[:, :, 3] = board.board .== EMPTY
+    result
+end
+
+function ones(board::Board)
+    fill!(BitArray(N, N), true)
+end
+
+function zeros(board::Board)
+    fill!(BitArray(N, N), false)
+end
+
+function turns_since(board::Board; maxval=8)
+    one_hot(board.cmove - board.order, maxval=maxval)
+end
+
+function liberties(board::Board; maxval=8)
+    liberties = Array{Int}(N,N)
+    for x in 1:N
+        for y in 1:N
+            point = Point(y,x)
+            group = board.groups[Point(y,x)]
+            liberties[point] = length(group.liberties)
+        end
+    end
+    one_hot(liberties, maxval=maxval)
+end
+
+# Liberties after move
+# Capture size of move
+# self atari size of move
+function after_move_features(board::Board; maxval=8)
+    legal = legal_moves(board)
+    liberties = zeros(Int, N, N)
+    groupsize = zeros(Int, N, N)
+    capture_size = zeros(Int, N, N)
+    color = current_player(board)
+    for move in legal
+        group = board.groups[move]
+        (friends, foes, empties) = get_friend_foe(board, move, color)
+        liberties[move] = length(union(group.liberties, [f.liberties for f in friends]...))
+        groupsize[move] = 1 + sum([length(f.members) for f in friends])
+        for foe in foes
+            if length(foe.liberties) == 1
+                capture_size[move] = length(foe.members)
+            end
+        end
+    end
+    (one_hot(liberties, maxval=maxval),
+    one_hot(groupsize, maxval=maxval),
+    one_hot(capture_size, maxval=maxval))
+end
+
+function ladder_capture(board::Board)
+end
+
+function ladder_escape(board::Board)
+end
+
+function sensibleness(board::Board)
+end
+
+function player_color(board::Board)
+    is_black = current_player(board) == BLACK
+    fill!(BitArray(N, N), is_black)
+end
+
+function get_features(board::Board)
+    features = Vector{BitArray}()
+    push!(features, stone_color(board))
+    push!(features, ones(board))
+    push!(features, zeros(board))
+    push!(features, turns_since(board))
+    push!(features, liberties(board))
+    push!(features, after_move_features(board)...)
+    push!(features, player_color(board))
+    return cat(3, features...)
+end
+
+b = Board()
+get_features(b)
---
+++ src/training.jl
@@ -0,0 +1,99 @@
+using HDF5
+
+function move_onehot(move::Point)
+    board = BitArray(N, N)
+    fill!(board, false)
+    board[linearindex(move)] = true
+    board
+end
+
+function generate_training_data(filename::AbstractString)
+    sgf = SGF(filename)
+    examples = Vector{Tuple{BitArray, BitArray}}()
+    if sgf == nothing
+        return examples
+    end
+    board = Board()
+    while true
+        move = get_next_move(sgf)
+        if move == nothing
+            break
+        end
+        if move != PASS_MOVE
+            features = get_features(board)[:]
+            answer = move_onehot(move)[:]
+            push!(examples, (features, answer))
+        end
+        play_move(board, move)
+    end
+    return examples
+end
+
+
+function generate_training_data(filenames::Vector{AbstractString}; progress_update=100)
+    examples = Vector{Tuple{BitArray, BitArray}}()
+    tm = time()
+    for (i, filename) in enumerate(filenames)
+        if i % progress_update == 0
+            println(STDERR, "$(i)/$(length(examples)): $(time() - tm)")
+        end
+        push!(examples, generate_training_data(filename)...)
+    end
+    examples
+end
+
+function write_hdf5(filename::AbstractString, examples::Vector{Tuple{BitArray, BitArray}})
+    data = hcat([x[1] for x in examples]...)
+    label = hcat([x[2] for x in examples]...)
+    h5open(filename, "w") do file
+        write(file, "data", 1.0 * data)
+        write(file, "label", 1.0 *label)
+    end
+end
+
+# Utility function
+# Use to filter out games between low ranked players
+function players_over_rating(filename::AbstractString, minrating::Int)
+    open(filename) do f
+        lines = readall(f)
+        wrating_regex = r"WR\[([0-9]{4})\]"
+        brating_regex = r"BR\[([0-9]{4})\]"
+        mw = match(wrating_regex, lines)
+        mb = match(brating_regex, lines)
+        if mw == nothing || mb == nothing
+            return false
+        end
+        if parse(Int, mw.captures[1]) < minrating || parse(Int, mb.captures[1]) < minrating
+            return false
+        end
+        return true
+    end
+end
+
+# find sgf files in a folder
+function find_sgf(folder::AbstractString)
+    files = Vector{AbstractString}()
+    folders = Vector{AbstractString}()
+    push!(folders, folder)
+    while length(folders) > 0
+        cpath = pop!(folders)
+        contents = readdir(cpath)
+        for entry in contents
+            path = joinpath(cpath, entry)
+            if isdir(path)
+                push!(folders, path)
+            elseif endswith(path, ".sgf")
+                push!(files, path)
+            end
+        end
+    end
+    files
+end
+
+
+function generate_and_save_examples(folder::AbstractString, out_hf5_path::AbstractString)
+    files = find_sgf(folder)
+    filtered_files =  filter(x -> players_over_rating(x, 2000), files)
+    examples = generate_training_data(filtered_files)
+    write_hdf5(out_hf5_path, examples)
+end
