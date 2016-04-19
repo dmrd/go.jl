ecfc2ef3dadc5508f49d6804440db5e46200a049
David Dohan <ddohan@cs.princeton.edu>
2016-04-19 09:11:21 +0000

    Fix number args in training example

--- examples/training.jl
+++ examples/training.jl
@@ -1,6 +1,6 @@
 using go
 
-if length(ARGS) != 1
+if length(ARGS) != 2
     println(STDERR, "usage: training.jl MODEL_NAME num_epochs")
     exit()
 end
@@ -14,7 +14,7 @@ println(STDERR, "Finding SGF files...")
 files = go.find_sgf("../data/cgs/");
 
 println(STDERR, "Finding SGF games with highly ranked players...")
-filtered_files =  filter(x -> go.players_over_rating(x, 2200), files)
+filtered_files =  filter(x -> go.players_over_rating(x, 2500), files)
 
 println(STDERR, "Extracting features...")
 examples = go.generate_training_data(filtered_files; progress_update=500, features=feats)
