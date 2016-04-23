2ffc3bd80a1cdfd14355320a129d36a9e304b1bf
David Dohan <ddohan@cs.princeton.edu>
2016-04-23 14:29:01 -0700

    Simplest of end-to-end tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
+#TODO: Real tests =(
 using go
 using Base.Test
 
-# write your own tests here
 @test 1 == 1
+
+# Very simple end-to-end sanity test
+# Fits a linear model to examples from a single SGF file.
+# Not going to be accurate, but tests simple 
+examples = go.generate_training_data("./example.sgf")
+X = go.batch_training_examples([x[1] for x in examples])
+Y = go.batch_training_examples([x[2][:] for x in examples])
+clf = go.CLF_LINEAR(go.LIBERTIES)
+go.train_model(clf, X, Y, epochs=100, validation_split=0.5)
+b = go.Board()
+move = go.choose_move(b, clf)
